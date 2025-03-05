bb200ed700fa162c6b4040d4da34a647dc70baea
Levanth93 <lavanthm@gmail.com>
2025-03-05 10:29:33 +0530

    Updated Sidebar and Main components, modified context and styles

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -9,13 +9,20 @@ const ContextProvider = (props) => {
 
     const [input, setInput] = useState("");
     const [recentPrompt,setRecentPrompt] = useState("");
-    const [prePrompts,setPrePrompts] = useState([]);
+    const [prevPrompts,setPrevPrompts] = useState([]);
     const [showResult,setShowResult] = useState(false);
     const [loading,setLoading] = useState(false);
     const [resultData,setResultData] = useState("");
 
     const delayPara = (index,nextWord) => {
+        setTimeout(function (){
+            setResultData(prev=>prev+nextWord);
+        },75*index)
+    }
 
+    const newChat =() => {
+        setLoading(false)
+        setShowResult(false)
     }
 
 
@@ -24,10 +31,18 @@ const ContextProvider = (props) => {
         setResultData("")
         setLoading(true)
         setShowResult(true)
-        setRecentPrompt(input)
-        const response = await run(input) 
+        let response;
+        if(prompt !== undefined){
+            response =await run(prompt);
+            setRecentPrompt(prompt)
+        }
+        else{
+            setPrevPrompts(prev=>[...prev,input])
+            setRecentPrompt(input)
+            response = await run(input)
+        }
         let responseArray = response.split("**");
-        let newResponse ;
+        let newResponse="";
         for(let i=0;i<responseArray.length;i++){
             if(i===0 || i%2 !==1){
             newResponse+=responseArray[i];
@@ -37,7 +52,11 @@ const ContextProvider = (props) => {
             }
         }
         let newResponse2= newResponse.split("*").join("</br>")
-        setResultData(newResponse2)
+        let newResponseArray = newResponse2.split(" ");
+        for(let i=0;i<newResponseArray.length;i++){
+            const nextWord =newResponseArray[i];
+            delayPara(i,nextWord+" ");
+        }
         setLoading(false)
         setInput("")
 
@@ -45,8 +64,8 @@ const ContextProvider = (props) => {
 
 
     const ContextValue = {
-        prePrompts,
-        setPrePrompts,
+        prevPrompts,
+        setPrevPrompts,
         onSent,
         setRecentPrompt,
         recentPrompt,
@@ -55,6 +74,7 @@ const ContextProvider = (props) => {
         resultData,
         input,
         setInput,
+        newChat
     };
 
     return (
